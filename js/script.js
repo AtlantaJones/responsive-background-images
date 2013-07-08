21b6f82de196893b677854eba09a9abe0f73f0b4
Andrew Jones <andrew@atlantajones.com>
2013-07-07 23:40:38 -0400

    support for medium images in place; misc tweaks

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 
+// set up some vars for breakpoints
+var bpMedium = '480px';
+var bpLarge = '1024px';
+
 
 // on resize, flip between small, medium or large image sizes on
 // any items containing the appropriate data attributes
 function picSwap() {
+
     // get all elements with a 'data-lowres' attribute
     var elements = $('*[data-lowres]');
 
@@ -17,12 +22,18 @@ function picSwap() {
             var domain = window.location.protocol + '//' + window.location.host + '/';
             currentBG = currentBG.replace(domain, '');
 
-            var lowRes = $(el).data('lowres');
-            var highRes = $(el).data('highres');
+            var lowRes      = $(el).data('lowres');
+            var mediumRes   = $(el).data('mediumres');
+            var highRes     = $(el).data('highres');
 
-            // if we're at 481px or over, load high-res
-            if(Modernizr.mq('screen and (min-width:481px)')) {
-                if(currentBG !== highRes) { // if it hasnt' changed, do nothing
+            // load high-res
+            if(Modernizr.mq('screen and (min-width: ' + bpMedium + ') and (max-width: ' + bpLarge + ')')) {
+                if(currentBG !== mediumRes) { // if it hasn't changed, do nothing
+                    $(el).css('background-image','url(' +  mediumRes + ')');
+                }
+
+            } else if(Modernizr.mq('screen and (min-width: ' + bpLarge + ')')) {
+                if(currentBG !== highRes) { // if it hasn't changed, do nothing
                     $(el).css('background-image','url(' +  highRes + ')');
                 }
             } else {
